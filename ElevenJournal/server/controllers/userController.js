f02f91a1ed4d110d7cb2a1eeb82883f17dd8791c
Abel Watson <80369480+abelwatson@users.noreply.github.com>
2021-06-11 18:42:23 -0400

    Blue Badge Server Section 9

---
+++ ElevenJournal/server/controllers/userController.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const { UserModel } = require("../models");
+
+router.post("/register", async (req,res) => {
+
+    let { email, password } = req.body.user;
+
+    UserModel.create({
+        email,
+        password
+    })
+})
+
+module.exports = router;
+
